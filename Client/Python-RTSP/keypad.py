434980ccc5f93b248b001c73033c8650624579f4
Luke Hart <luke2.hart@live.uwe.ac.uk>
2014-04-28 22:37:50 +0100

    working 7segs and keypad standalone

--- Client/Python-RTSP/keypad.py
+++ Client/Python-RTSP/keypad.py
@@ -1,76 +1,109 @@
 import serial
 import time
 import io
+import threading
 
 TTY = '/dev/ttyACM0'
 BAUD = 115200
 
 START = '@00'
 END = '\r'
-PORTA_OUTPUT = '@00D000\r' #port A/0 output
-KEY_INPUT = '@00D1FF\r'  # port B/1 input
-LED_OUTPUT = '@00D200\r' # port C/2 output
+A = 0
+B = 1
+C = 2
+OUTPUT = 0
+INPUT = 1
+#A - Both B - KeyPad C - 7 SEG LEDS  
+PORTABC_INPUT = ['@00D0FF\r','@00D1FF\r', '@00D2FF\r']
+PORTABC_OUTPUT = ['@00D000\r','@00D100\r', '@00D200\r'] 
+
 SELECT_COLUMN = ['@00P001\r','@00P002\r','@00P004\r','@00P008\r'] #select first column
 #"@00P23F\r" write hex code for a 0
 CHECK_BUTTON = '@00P1?\r'
-SEG_7 = ['01', '02', '04' ,'08']
+
 
 class PIO:
     def __init__(self):
         self.ser = serial.Serial(TTY, baudrate=BAUD, timeout=1)
-        self.write(PORTA_OUTPUT)
-        self.write(KEY_INPUT)
-        self.write(LED_OUTPUT)
-    def setup_write(self, display):
-        self.write('@00P2' + '00' + END ) # write hex code for a 0
+        self.port_setup(A, OUTPUT)
+        time.sleep(0.2)
+        self.port_setup(B, INPUT)
+        time.sleep(0.2)
+        self.port_setup(C, OUTPUT)
+        time.sleep(0.2)
+        
+        self.ser_io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser, 1),  
+                              newline = '\r',
+                             line_buffering = True)   
+
+    def port_setup(self, port, io):
+        if (io == OUTPUT): 
+            self.ser.write(PORTABC_OUTPUT[port])
+        elif (io == INPUT):
+            self.ser.write(PORTABC_INPUT[port])
+        else:
+            print "Invalid IO choice"
+        
+    def write(self,cmd):
+        ##Should error check
+        self.ser_io.write(unicode(cmd))
+        self.ser_io.flush()
+        return self.ser_io.readline()  
+    
+    def setup_display(self, display):
+        # Clear first
+        self.write('@00P200\r') 
         time.sleep(0.0001)
-        keypad.write(START+'P0'+display+END) # port C/2 ou
+        # now select output display
+        self.write(SELECT_COLUMN[display]) 
         time.sleep(.001)
         
-    def write(self, cmd):
-        self.ser.write(cmd)
-    def setup_read(self):
-        self.ser_io = None
-        self.ser.write(KEY_INPUT)
-        time.sleep(.1)
-        if(self.ser_io is None):
-            self.ser_io = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser, 1),  
-                               newline = '\r',
-                               line_buffering = True)
-        time.sleep(.1)
-    def read(self):
-        self.setup_read()
-        keys = [0,0,0,0]
-        for i in range(0,4):
-            self.write(SELECT_COLUMN[i])
-            time.sleep(.01)
-            self.ser.write(CHECK_BUTTON)
-            time.sleep(0.001)
-            key = str(self.ser_io.readline())
-            key = key.lstrip('!')
-            key = key.rstrip()
-            print key
-            if(int(key) != 0): 
-                keys[i] = int(key)
-        return keys
-    # params: num[4] array of size 4 which takes numbers from 0-9 for each lcd item
-    # ToDo: Could just be a number 0-9999 and be broken into components but for now
-    # this works
+    # 
     def display(self, num):
+        """
+        params: num[4] array of size 4 which takes numbers from 0-9 for each lcd item
+        ToDo: Could just be a number 0-9999 and be broken into components but for now
+        this works
+        """
+        x = 0
+        ssd = 0
+                   
         #Write values for appox 1 second
-        for x in range (0, 100):
-            for i in range (0,4):
-                self.setup_write(SEG_7[i])
-                self.write('@00P2' + self.ledSwitch(str(num[i])) + END )
-                time.sleep(0.001)
+        for x in range(0, 100):
+            for ssd in range (0,4):
+                self.setup_display(ssd)
+                self.write('@00P2' + self.ledSwitch(str(num[ssd])) + END )
+                #Change this value for slower platfroms.. this is for a fast pc.. try 0.0001 for rpi to start with
+                time.sleep(0.005)
+                
         #Clear display at finish
-        for i in range (0,4):
-            self.setup_write(SEG_7[i])
-        
+        for disp in range (0,4):
+            self.setup_display(disp)
+            time.sleep(0.1)
+                 
         
     def close(self):
         self.ser.close()
-    def ledSwitch (self, choice):
+    def keypad_read(self):
+        keys = [0,0,0,0]
+        for i in range(0,4):
+            self.write(SELECT_COLUMN[i])
+            time.sleep(0.001)
+            key = self.write(CHECK_BUTTON)
+            time.sleep(0.001)
+            #keyInit = self.ser_io.readlines()
+            self.ser_io.flush()
+            
+            key = key.lstrip('!')
+            key = key.rstrip()
+                                   
+            try:
+                if(int(key) != 0): 
+                    keys[i] = int(key)
+            except:
+                continue
+        return keys
+    def ledSwitch(self, choice):
             return {
                 '0' : '3f',
                 '1' : '06',
@@ -89,21 +122,33 @@ class PIO:
                 'E' : '79',
                 'F' : '71',
             }[choice]
+    def keypadSwitch(self, column, value):
+        if (column == 3 ):
+            print "Col 1 But " + str(value) 
+        if (column == 2 ):
+            print "Col 2 But " + str(value)
+        if (column == 1 ):
+            print "Col 3 But " + str(value)
+        if (column == 0 ):  
+            print "Col 4 But " + str(value)
 
 
 if __name__ == '__main__':
     
-    keypad = PIO()
-        
+    pio = PIO()
+    #thread = threading.Thread(target=read_from_port, args=(serial_port,))    
 #    test calls for lcd write
-    #keypad.display([1, 2, 3, 4])
-    #keypad.display([0, 4, 5, 9])
-    #keypad.display([5, 0, 9, 4])     
+    #pio.display([1, 2, 3, 4])
+    #pio.display([0, 'C', 5, 9])
+    #pio.display([5, 0, 'A', 4])
+    #pio.display([1, 2, 3, 4])     
     
     while(1):
-        keys = keypad.read()
-        for i in range(0,4):
-            if (keys[i] > 0):
-                print keys[i] 
+        keys = pio.keypad_read()
+        print keys
+        for col in range (0,4):
+            print keys[col]
+            if (keys[col] > 0):
+                pio.keypadSwitch(col, keys[col])                                         
     
-    keypad.close()
+    pio.close()
